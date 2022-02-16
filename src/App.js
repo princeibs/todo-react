f72f6c266b1f89d76cb0c70ea81cca481f1e52eb
Suleiman Ibrahim <suleimanonnet@gmail.com>
2022-02-16 08:45:43 +0100

    Base commit with project structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div id="title">TODO</div>
+        <input type="text" placeholder="Create a new todo ..." />
       </header>
+      <div class="main">
+        <div class="nav">
+          <span class="items-count">2 items left</span>
+          <span>
+            <button>All</button>
+          </span>
+          <span>
+            <button>Active</button>
+          </span>
+          <span>
+            <button>Completed</button>
+          </span>
+        </div>
+        <div class="todo-list">
+          <div class="todo-item">
+            <input type="checkbox" />
+            <div class="todo-text">This is a new todo item</div>
+            <button class="close-todo-item">x</button>
+          </div>
+          <div class="todo-item">
+            <input type="checkbox" />
+            <div class="todo-text">This is a new todo item</div>
+            <button class="close-todo-item">x</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
